eb6caf0c8de16e89108dc5185bdb7a04dbaaa05a
nammba <pvitulano@gmail.com>
2022-11-08 06:42:53 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 let number_of_leds = 144
+let number_of_leds_of_range = 144
 let strip = neopixel.create(DigitalPin.P0, number_of_leds, NeoPixelMode.RGB)
 strip.setBrightness(20)
-let range = strip.range(0, 10)
-range.showColor(neopixel.colors(NeoPixelColors.Green))
+let range = strip.range(0, number_of_leds_of_range)
 basic.forever(function () {
-    strip.rotate(1)
+    for (let index = 0; index <= number_of_leds_of_range; index++) {
+        range.setPixelColor(index, neopixel.hsl(3970 + randint(-10, 40), 100, 20))
+    }
     strip.show()
-    basic.pause(10)
 })
